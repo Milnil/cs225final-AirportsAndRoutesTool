c82795f326e0c677ba87afeb91219083ce767578
Milnil <mphilar@gmail.com>
2022-11-09 23:14:47 +0000

    Created testing and stuff along with the airport node class

---
+++ build/test_tests-b12d07c.cmake
@@ -0,0 +1,3 @@
+add_test( addElements1 /workspaces/cs225/cs225final-nerds/build/test addElements1  )
+set_tests_properties( addElements1 PROPERTIES WORKING_DIRECTORY /workspaces/cs225/cs225final-nerds/build)
+set( test_TESTS addElements1)
